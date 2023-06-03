c833b2ed6af5a3b9bb3db5f107efb28e24d1e11a
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-06-03 19:34:57 +0300

    03.06.2023..

--- src/test/java/com/myapp/utilities/DataProviderUtils.java
+++ src/test/java/com/myapp/utilities/DataProviderUtils.java
@@ -1,9 +1,6 @@
 package com.myapp.utilities;
-
 import org.testng.annotations.DataProvider;
-
 public class DataProviderUtils {
-
     @DataProvider
     public Object[][] customerCredentials(){
 //        This is a data source
@@ -14,5 +11,24 @@ public class DataProviderUtils {
         };
         return customerInfo;
     }
-
+    //    DATA FOR LOGIN TEST
+    @DataProvider
+    public Object[][] customerDataProvider(){
+        Object [][] customerInfo={
+                {"sam.walker@bluerentalcars.com","c!fas_art"},
+                {"kate.brown@bluerentalcars.com","tad1$Fas"},
+                {"raj.khan@bluerentalcars.com","v7Hg_va^"},
+                {"pam.raymond@bluerentalcars.com","Nga^g6!"}
+        };
+        return customerInfo;
+    }
+    //    THIS DATA SOURCE WILL GET THE DATA FROM THE EXCEL SHEET
+    @DataProvider
+    public Object[][] excelCustomerDataProvider(){
+        String path = "./src/test/java/resources/mysmoketestdata.xlsx";
+        String sheetName = "customer_info";
+        ExcelUtils excelUtils = new ExcelUtils(path,sheetName);
+        Object[][] customerInfo = excelUtils.getDataArrayWithoutFirstRow();
+        return customerInfo;
+    }
 }
