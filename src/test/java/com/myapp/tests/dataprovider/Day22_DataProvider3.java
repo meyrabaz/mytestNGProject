0b045d5a2e710cd41f0b7c8f5f6236e812988c35
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-06-07 12:41:08 +0300

    07.06.2023

---
+++ src/test/java/com/myapp/tests/dataprovider/Day22_DataProvider3.java
@@ -0,0 +1,47 @@
+package com.myapp.tests.dataprovider;
+import com.myapp.pages.RentalHomePage;
+import com.myapp.pages.RentalLoginPage;
+import com.myapp.utilities.*;
+import org.testng.annotations.Test;
+public class Day22_DataProvider3 {
+  //    TEST LOGIN FUNCTIONALITY USING CUSTOMER SERVICE CREDENTIALS
+//    We need 2 methods : ****@DataProvider ***@Test method
+  RentalHomePage rentalHomePage;
+  RentalLoginPage rentalLoginPage;
+  @Test(dataProvider = "customerDataProvider",dataProviderClass = DataProviderUtils.class)
+  public void customerLoginTest(String username,String password){
+    System.out.println(username + password);
+    Driver.getDriver().get(ConfigReader.getProperty("rental_home_url"));
+    rentalHomePage = new RentalHomePage();
+    rentalLoginPage = new RentalLoginPage();
+//            LOG IN
+//            click on login
+    WaitUtils.waitFor(1);
+    rentalHomePage.loginLink.click();
+    WaitUtils.waitFor(1);
+//            enter username
+    rentalLoginPage.userEmail.sendKeys(username);//GET DATA FROM DATA PROVIDER
+    WaitUtils.waitFor(1);
+//            enter password
+    rentalLoginPage.userPassword.sendKeys(password);//GET DATA FROM DATA PRIVIDER
+    WaitUtils.waitFor(1);
+//            click on login button
+    rentalLoginPage.loginButton.click();
+    WaitUtils.waitFor(1);
+//            verify login
+    ReusableMethods.verifyElementDisplayed(rentalHomePage.userID);
+    WaitUtils.waitFor(1);
+//            LOG OUT
+//            click on id
+    rentalHomePage.userID.click();
+    WaitUtils.waitFor(3);
+//            click on logout
+    rentalHomePage.logOut.click();
+    WaitUtils.waitFor(1);
+//            click on ok
+    rentalHomePage.OK.click();
+    WaitUtils.waitFor(1);
+    Driver.closeDriver();
+  }
+
+}
