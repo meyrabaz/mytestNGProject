c40ef414d710a28a1b4ffee91a492915445d1a5b
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-06-09 21:09:30 +0300

    09.06.2023

---
+++ src/test/java/com/myapp/pages/RentalHomePage.java
@@ -0,0 +1,18 @@
+package com.myapp.pages;
+import com.myapp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class RentalHomePage {
+    public RentalHomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(partialLinkText = "Login")
+    public WebElement loginLink;
+    @FindBy(id = "dropdown-basic-button")
+    public WebElement userID;
+    @FindBy(linkText = "Logout")
+    public WebElement logOut;
+    @FindBy(xpath = "//button[.='OK']")
+    public WebElement OK;
+}
